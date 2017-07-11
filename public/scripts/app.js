53a537f2079d3bae0c260aa2ad45a5be5b84ea2a
mstop4 <mstop4@gmail.com>
2017-07-11 21:47:53 +0000

    can read A tweet from database

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,69 +4,32 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-let tweets = {
-  "user": {
-    "name": "Newton",
-    "avatars": {
-      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-    },
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-}
+function createTweetElement(tweet) {
 
-// Example tweet HTML
-
-// <article class="tweet-container">
-
-//   <header>
-//     <img class="avatar" src="images/books-icon.png">
-//     <span class="username">Lorem Ipsum</span>
-//     <span class="handle">@lorem_ipsum</span>
-//   </header>
-
-//   <div class="tweet-body">
-//     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nisi velit, hendrerit ut auctor nec, placerat id elit.<br/><span class="hashtag">#loremipsum</span></p>
-//   </div>
-
-//   <footer>
-//     <p>1000 days ago</p>
-//     <span class="iconset">
-//       <img src="images/003-black.png"/>
-//       <img src="images/002-arrows.png"/>
-//       <img src="images/001-like-of-filled-heart.png"/>
-//     </span>
-//   </footer>
-
-// </article>
-
-function createTweetElement() {
+  console.dir(tweet);
 
   let $newTweet = $("<article></article>").addClass("tweet-container");
 
   let $header = $("<header></header>");
   $newTweet.append($header);
 
-  $header.append($("<img/>").addClass("avatar").attr("src", "images/books-icon.png"));
-  $header.append($("<span></span>").addClass("username").text("Lorem Ipsum"));
-  $header.append($("<span></span>").addClass("handle").text("@lorem_ipsum"));
+  $header.append($("<img/>").addClass("avatar").attr( "src", tweet["user"]["avatars"]["large"] ));
+  $header.append($("<span></span>").addClass("username").text(tweet["user"]["name"]));
+  $header.append($("<span></span>").addClass("handle").text(tweet["user"]["handle"]));
 
   let $tweetBody = $("<div></div>").addClass("tweet-body");
   $newTweet.append($tweetBody);
 
-  $tweetBody.append($("<p></p>").text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nisi velit, hendrerit ut auctor nec, placerat id elit. #loremipsum"));
+  $tweetBody.append($("<p></p>").text(tweet["content"]["text"]));
 
   let $footer = $("<footer></footer");
   $newTweet.append($footer);
+  let currentTime = new Date(Date.now());
+  let timeAgo = Math.floor((currentTime - tweet["created_at"]) / 1000);
 
-  $footer.append($("<p></p>").text("1000 days ago"));
+  $footer.append($("<p></p>").text(timeAgo + " seconds ago"));
 
-  let $iconset = $("<span></span>").addClass("iconset")
+  let $iconset = $("<span></span>").addClass("iconset");
   $footer.append($iconset);
 
   $iconset.append($("<img/>").attr("src", "images/003-black.png"));
@@ -76,11 +39,8 @@ function createTweetElement() {
   $('.tweet-list').append($newTweet);
 }
 
-createTweetElement();
+function renderTweets(tweetsList) {
+  for (var i = 0; i < tweetList.length; i++) {
 
-// function renderTweets(tweetsList) {
-
-//   for (var i = 0; i < tweetList.length; i++) {
-
-//   }
-// }
+  }
+}
