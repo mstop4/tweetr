e39a72caa4f803bb531dd83ba54babcf8960c033
mstop4 <mstop4@gmail.com>
2017-07-11 15:30:40 +0000

    character counter works fully

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,28 @@
 console.log("I am composer-char-counter.js");
 
+// Keeps track of characters remaining
+function countCharacters() {
+
+  const maxChars = 140;
+  let charsLeft = maxChars - context.val().length;
+  let counterLabel = context.closest(".new-tweet").find(".counter");
+
+  counterLabel.text(charsLeft);
+
+  // If over the 140-character limit, make the text red.
+  // Otherwise, reset it to black
+  if (charsLeft < 0 ) {
+    counterLabel.addClass("charOverflow");
+  } else {
+    counterLabel.removeClass("charOverflow");
+  }
+}
+
 $(document).ready( function() {
   console.log("composer-char-counter.js is ready.");
 
   // Typing in "Compose Tweet" text box:
-  // Keeps track of characters remaining
-  $(".new-tweet").on("keypress", "textarea", function() {
-
-    let charsLeft = 140 - $(this).val().length;
-    let counterLabel = $(this).closest(".new-tweet").find(".counter");
-    counterLabel.text(charsLeft);
-
-  });
+  // Updates character counter as you type
+  $(".new-tweet").on("input", "textarea", function () { countCharacters(); });
 });
 
