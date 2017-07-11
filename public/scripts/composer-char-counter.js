d8f894bf47171b470a0a52c81166df4d58a6289f
mstop4 <mstop4@gmail.com>
2017-07-11 14:42:02 +0000

    added event handler to textarea

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,4 +2,11 @@ console.log("I am composer-char-counter.js");
 
 $(document).ready( function() {
   console.log("I'm ready");
+
+  // I'm typing
+  $(".new-tweet").on("keypress", "textarea", function() {
+    console.log("SDFDSF");
+  })
+
 });
+
